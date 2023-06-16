9dd9f76b1471afc8d434012e2392247b42122905
helmy <mohamedhelmy231@gmail.com>
2023-06-15 18:56:48 -0700

    A

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: A C program that prints with the puts function
+ * Description: A C program that prints with printf function.
  *
- * Return: Always 0 (Success)
- */
-int main()
+ * Return: Always 0 (success)
+*/
+
+int main(void)
 {
-printf(with proper grammar, but the outcome is a piece of art,/n);
-return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
