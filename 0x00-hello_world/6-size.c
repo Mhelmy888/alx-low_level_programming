36f05eefd1222dae5dde5bb8b829fec3a02376d0
helmy <mohamedhelmy231@gmail.com>
2023-06-15 19:42:07 -0700

    A

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <inttypes.h>
 
 /**
  * main - Entry point
@@ -10,14 +9,14 @@
  */
 int main(void)
 {
-    printf("Size of char: %" PRIuPTR " byte(s)\n", sizeof(char));
-    printf("Size of short: %" PRIuPTR " byte(s)\n", sizeof(short));
-    printf("Size of int: %" PRIuPTR " byte(s)\n", sizeof(int));
-    printf("Size of long: %" PRIuPTR " byte(s)\n", sizeof(long));
-    printf("Size of long long: %" PRIuPTR " byte(s)\n", sizeof(long long));
-    printf("Size of float: %" PRIuPTR " byte(s)\n", sizeof(float));
-    printf("Size of double: %" PRIuPTR " byte(s)\n", sizeof(double));
-    printf("Size of long double: %" PRIuPTR " byte(s)\n", sizeof(long double));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of short: %lu byte(s)\n", sizeof(short));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of double: %lu byte(s)\n", sizeof(double));
+	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
 
-    return 0;
+	return 0;
 }
