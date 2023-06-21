8d52c06ecbe834c17818e00e0feaf0e7ff405113
helmy <mohamedhelmy231@gmail.com>
2023-06-21 11:57:39 -0700

    A

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,39 +1,22 @@
 #include <stdio.h>
 
 /**
- * print_times_table - Prints the times table of the input.
- * @n: The value of the times table to be printed.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-void print_times_table(int n)
+int main(void)
 {
-	if (n < 0 || n > 15)
-		return;
+	int i;
+	int sum = 0;
 
-	int num, mult, prod;
-
-	for (num = 0; num <= n; num++)
+	for (i = 0; i < 1024; i++)
 	{
-		for (mult = 1; mult <= n; mult++)
-		{
-			prod = num * mult;
-
-			if (prod <= 9)
-			{
-				printf("  %d", prod);
-			}
-			else if (prod <= 99)
-			{
-				printf(" %d", prod);
-			}
-			else
-			{
-				printf("%d", prod);
-			}
-
-			if (mult < n)
-				printf(",");
-		}
-
-		printf("\n");
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
 	}
+
+	printf("%d\n", sum);
+
+	return (0);
 }
