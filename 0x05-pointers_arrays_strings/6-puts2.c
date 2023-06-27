eef45fdab740a1b9ad7b3cd8a23709aaf505dccf
helmy <mohamedhelmy231@gmail.com>
2023-06-27 13:23:01 -0700

    A

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,20 +5,19 @@
  * @str: The string to be treated
  *
  * Description: This function iterates through the string and prints every other
- *              character, starting with the first character, until it reaches
- *              the null terminator.
+ * character, starting with the first character, until it reaches the null terminator.
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            _putchar(str[i]);
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
 
-        i++;
-    }
+		i++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
