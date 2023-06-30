25f1d146741a6282e60b5c38ff8e3fe894c8f061
helmy <mohamedhelmy231@gmail.com>
2023-06-29 17:59:14 -0700

    A

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,24 +9,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int dest_len = 0;
-    int i;
+	int dest_len = 0;
+	int i;
 
-    /* Find the length of the destination string */
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
+	/* Find the length of the destination string */
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
 
-    /* Append the source string to the destination string */
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        dest[dest_len] = src[i];
-        dest_len++;
-    }
+	/* Append the source string to the destination string */
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+	}
 
-    /* Add the terminating null byte */
-    dest[dest_len] = '\0';
+	/* Add the terminating null byte */
+	dest[dest_len] = '\0';
 
-    return dest;
+	return (dest);
 }
