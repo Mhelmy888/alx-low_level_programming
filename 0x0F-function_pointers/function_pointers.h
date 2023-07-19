5b03fd99c90073c4094101714030b5ccd472e085
helmy <mohamedhelmy231@gmail.com>
2023-07-19 09:28:01 -0700

    A

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
-#define FUNCTION POINTERS H
-#include ‹stdlib.h>
-void print_name(char *name, void (*f) (char *));
-void array_iterator (int *array, size_t size, void (*action) (int));
-int int _index(int *array, int size, int (*cmp) (int));
-#endif
+#define FUNCTION_POINTERS_H
+
+#include <stdlib.h> // Include the necessary standard library header
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif /* FUNCTION_POINTERS_H */
