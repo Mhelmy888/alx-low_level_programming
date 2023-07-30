b0679c733e36bb924cbc1121d534a5ad03807f8c
helmy <mohamedhelmy231@gmail.com>
2023-07-30 01:59:44 -0700

    readme

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int strlen_recursion(char *s);
+int factorial(int n);
+int pow_recursion(int x, int y);
+int sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif /* MAIN_H */
