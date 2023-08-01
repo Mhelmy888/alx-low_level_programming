214739a9aaef7a711fe47f2fb05b210c199f7597
helmy <mohamedhelmy231@gmail.com>
2023-08-01 08:51:42 -0700

    readme

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,28 @@
+/*
+ * 0x13. C - More singly linked lists
+ * task 0
+ * my solution depends on looping and change h "head" reference with the next h
+ * and check if it equals null so it is the end
+ */
 #include "lists.h"
-
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * print_listint - print list of ints
+ * @h: refrance of list_t "linked list of ints"
+ * Return: list size
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+	int size;
 
-    while (h)
-    {
-        printf("%d\n", h->n);
-        num++;
-        h = h->next;
-    }
+	size = 0;
+	if (h == NULL)
+		return (0);
 
-    return (num);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		size++;
+	}
+	return (size);
 }
